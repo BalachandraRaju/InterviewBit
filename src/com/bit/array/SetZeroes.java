0537325c8c7da0db12f8a24568f88882d2d98d65
rock626 <geethabujji@gmail.com>
2017-08-15 12:58:33 -0700

    Update SetZeroes.java

--- src/com/bit/array/SetZeroes.java
+++ src/com/bit/array/SetZeroes.java
@@ -24,52 +24,107 @@ On returning, the array A should be :
  */
 public class SetZeroes {
 
-	public ArrayList<ArrayList<Integer>> setZeroes(ArrayList<ArrayList<Integer>> a) {
-		int R=1;
-		int C=1;
-		if(a.isEmpty()){
-			return a;
-		}
-		//check 1st row
-		for(int j=0;j<a.get(0).size();j++){
-			if(a.get(0).get(j) == 0){
-				R=0;
+	public static void setZeroes(ArrayList<ArrayList<Integer>> a) {
+		int r = 1;
+		int c = 1;
+		// check 1st column
+		for (int i = 0; i < a.size(); i++) {
+			if (a.get(i).get(0) == 0) {
+				c = 0;
 				break;
 			}
 		}
-		
-		//check first column
-		for(int i=0;i<a.size();i++){
-			if(a.get(i).get(0) == 0){
-				C=0;
+
+		// check 1st row
+		for (int j = 0; j < a.get(0).size(); j++) {
+			if (a.get(0).get(j) == 0) {
+				r = 0;
 				break;
 			}
 		}
+
+		// now check remaining rows
+		for (int i = 1; i < a.size(); i++) {
+			int x = a.get(i).get(0);
+			if (x == 0) {
+				a.get(i).set(0, 0);
+			} else {
+				for (int j = 1; j < a.get(i).size(); j++) {
+					if (a.get(i).get(j) == 0) {
+						a.get(i).set(0, 0);
+						break;
+					}
+				}
+			}
+		}
+
+		// now check remaining cols
+		for (int j = 1; j < a.get(0).size(); j++) {
+			int x = a.get(0).get(j);
+			if (x == 0) {
+				a.get(0).set(j, 0);
+			} else {
+				for (int i = 1; i < a.size(); i++) {
+					if (a.get(i).get(j) == 0) {
+						a.get(0).set(j, 0);
+						break;
+					}
+				}
+			}
+		}
+
 		
 		
-		
-		for(int i=1;i<a.size();i++){
-			for(int j=1;j<a.get(i).size();j++){
-				if(a.get(i).get(j)==0){
-					a.get(i).set(0,0);
-					break;
+
+
+		// set remaining rows n cols
+		for (int i = 1; i < a.size(); i++) {
+			for (int j = 1; j < a.get(i).size(); j++) {
+				if (a.get(0).get(j) == 1 && a.get(i).get(0) == 1) {
+					a.get(i).set(j, 1);
+				} else {
+					a.get(i).set(j, 0);
 				}
 			}
 		}
 		
-		for(int j=1;j<a.get(0).size();j++){
-			for(int i=1;i<a.size();i++){
-				if(a.get(i).get(j)==0){
-					a.get(0).set(j,0);
-					break;
-				}
-			}
+		// update 1st column
+		for (int i = 1; i < a.size(); i++) {
+			a.get(i).set(0,c);
 		}
-		return a;
+
+		// update 1st row
+		for (int j = 1; j < a.get(0).size(); j++) {
+			a.get(0).set(j,r);
+		}
+		// row 0 and col 0
+		int k = r & c;
+		a.get(0).set(0, k);
+
 	}
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Integer> row1 = new ArrayList<Integer>();
+		row1.add(1);
+		row1.add(0);
+		row1.add(1);
+		ArrayList<Integer> row2 = new ArrayList<Integer>();
+		row2.add(1);
+		row2.add(1);
+		row2.add(1);
+		ArrayList<Integer> row3 = new ArrayList<Integer>();
+		row3.add(1);
+		row3.add(1);
+		row3.add(1);
+
+		ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
+		matrix.add(row1);
+		matrix.add(row2);
+		matrix.add(row3);
+		System.out.println(Arrays.toString(matrix.toArray()));
+
+		setZeroes(matrix);
+		System.out.println(Arrays.toString(matrix.toArray()));
 
 	}
 
